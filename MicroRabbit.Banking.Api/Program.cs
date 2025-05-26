b19988efaf01045ee66db935bc9a69aac8de066d
Sunil Kumar <sunilpalarya@gmail.com>
2025-05-26 20:09:04 +0530

    Data layer completed with working migration

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -1,25 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        //builder.Services.AddMediatR(typeof(Program));
+
+        RegisterServices(builder.Services, builder.Configuration);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BankingDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("BankingDbConnection"));
+        });
+
+        DependencyContainer.RegisterServices(services);
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ MicroRabbit.Banking.Data/BankingDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using MicroRabbit.Banking.Data.Context;
+using System.IO;
+
+public class BankingDbContextFactory : IDesignTimeDbContextFactory<BankingDbContext>
+{
+    public BankingDbContext CreateDbContext(string[] args)
+    {
+        // Set up configuration to read from appsettings.json
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) // where EF is running from
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("BankingDbConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<BankingDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new BankingDbContext(optionsBuilder.Options);
+    }
+}
